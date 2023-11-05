6c5938f00169cc79cb836b5a88de886e69f53731
Nirajan1-droid <nirajanbhattarai12345@gmail.com>
2023-11-05 09:41:17 +0545

    anotherone

---
+++ src/anotherone.js
@@ -0,0 +1,5 @@
+function another(){
+    return "something";
+}
+
+module.export(another);
---
+++ src/merge.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Update local branch with remote changes
+git fetch
+git pull test master
+
+# Switch to your branch
+git checkout master
+
+# Merge changes from the main branch
+git merge main
+
+# Resolve conflicts if any
+# (You can add additional commands here)
+
+# Commit and push the changes
+git commit -m "Merge changes"
+git push origin your-branch
